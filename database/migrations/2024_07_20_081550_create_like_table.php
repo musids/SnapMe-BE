b4135386a7bbc55aecd8b38af94d6d844698e934
Liliauuaw <147015805+Liliauuaw@users.noreply.github.com>
2024-07-20 15:43:51 +0700

    Update database/migrations/2024_07_20_081550_create_like_table.php
    Co-authored-by: Dzikri Azzakah <46553030+DzikriAzzakah@users.noreply.github.com>

--- database/migrations/2024_07_20_081550_create_like_table.php
+++ database/migrations/2024_07_20_081550_create_like_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('like', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('user_id')->unique();
             $table->unsignedBigInteger('post_id');
             $table->timestamps();
         });
